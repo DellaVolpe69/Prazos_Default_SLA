83c8a433e5599d4a73713c116961df8ac4ee58f0
Della Volpe <tdvmatriz@gmail.com>
2025-12-04 11:06:47 -0300

    Update scr_Prazos_Default_SLA.py

--- scr_Prazos_Default_SLA.py
+++ scr_Prazos_Default_SLA.py
@@ -45,7 +45,7 @@ st.markdown("""
 <style>
 /* Forçar cor branca em qualquer texto dentro de markdown ou write */
 /* p, span, div, label { */
-div, label {
+span, label {
     color: #EDEBE6 !important;
 }
 </style>
